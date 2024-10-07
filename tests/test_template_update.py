04123fe38ab17595878c05967aef9096d741ec28
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2024-10-07 13:40:38 +0200

    .

--- tests/test_template_update.py
+++ tests/test_template_update.py
@@ -1,26 +1,37 @@
 # Disable line length check for this file
 # ruff: noqa: E501
 
-import os
 import shutil
+from pathlib import Path
 from textwrap import dedent
 
-from scripts.update_actions import update_project_workflows
+REPO_ROOT = Path(__file__).parent.parent
 
 
-def test_template_actions_update(tmp_path):
-    (tmp_path / "template/.github").mkdir(parents=True)
-    shutil.copytree(
-        "template/.github/workflows",
-        tmp_path / "template/.github/workflows",
-    )
+def custom_get_latest_github_tag(_action: str) -> tuple[str, str]:
+    return "v9.9.9", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
-    update_project_workflows(tmp_path / "template/.github/workflows", dry_run=True)
 
-    for file in os.listdir(tmp_path / "template/.github/workflows"):
-        with open(tmp_path / "template/.github/workflows" / file) as f:
-            with open(os.path.join("template/.github/workflows", file)) as g:
-                assert f.read() == g.read()
+def test_template_actions_update(tmp_path: Path):
+    # Arrange
+    src = REPO_ROOT / "template" / ".github" / "workflows"
+    dst = tmp_path / src.relative_to(REPO_ROOT)
+    dst.parent.mkdir(parents=True)
+    shutil.copytree(src, dst)
+    # Act
+    # need to import here for monkeypatch
+    from scripts.update_actions import update_project_workflows
+
+    update_project_workflows(str(dst.absolute()))
+
+    # Assert
+    for file in dst.iterdir():
+        observed = file.read_text()
+        if "prefix-dev/setup-pixi" in observed:
+            assert (
+                "prefix-dev/setup-pixi@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa # v9.9.9"
+                in observed
+            )
 
 
 def test_actions_update(tmp_path):
@@ -30,14 +41,15 @@ def test_actions_update(tmp_path):
             dedent(
                 """
                 jobs:
-                  test:
+                test:
                     steps:
-                      - uses: actions/upload-release-asset@v1
-                      - uses: helaili/jekyll-action@v1
-                      - uses: actions/upload-release-asset@64e5e85fc528f162d7ba7ce2d15a3bb67efb3d80
+                    - uses: actions/upload-release-asset@64e5e85fc528f162d7ba7ce2d15a3bb67efb3d80 # v0.0.0
                 """
             ).lstrip()
         )
+
+    # need to import here for monkeypatch
+    from scripts.update_actions import update_project_workflows
 
     update_project_workflows(tmp_path / "template/.github/workflows")
 
@@ -47,11 +59,9 @@ def test_actions_update(tmp_path):
             == dedent(
                 """
                 jobs:
-                  test:
+                test:
                     steps:
-                      - uses: actions/upload-release-asset@v1
-                      - uses: helaili/jekyll-action@v2
-                      - uses: actions/upload-release-asset@e8f9f06c4b078e705bd2ea027f0926603fc9b4d5
+                    - uses: actions/upload-release-asset@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa # v9.9.9
                 """
             ).lstrip()
         )
